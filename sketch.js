0ee62ca65497f02fff04c0bb9b6fc8d3d4ad7643
ednl <ewoud@dronkert.net>
2020-08-17 19:51:42 +0200

    Almost there

--- sketch.js
+++ sketch.js
@@ -1,63 +1,75 @@
-const DIMX = 800;
-const DIMY = 600;
+// Physical parameters
+const L  = 2.5;     // pendulum length in m
+const pm = 1;     // pendulum mass in kg
+const cm = 2;    // cart mass in kg
 
-const G     =   9.81;      // gravitational constant in m/s^2
-const DT    =   0.01;      // timestep per update in s
-const SCALE = 100;         // pixels per metre
-const EPS   =   0.000001;  // truncation threshold (epsilon)
+// Simulation parameters
+const DT = 0.01;  // timestep per update in s
+const G  = 9.81;  // gravitational constant in m/s^2
 
-const BOUNDX = DIMX * 0.5 / SCALE;  // max |x| in m
-const BOUNDY = DIMY * 0.5 / SCALE;  // max |y| in m
+// Visualisation parameters
+const DIMX  = 800;  // canvas width
+const DIMY  = 600;  // canvas height
+const SCALE = 100;  // pixels per metre
+const CARTW =  60;  // cart width in pixels
+const CARTH =  20;  // cart Height in pixels
+const PENDR =  10;  // pendulum radius in pixels
 
-let cart, pend;
+// Simulation variables
+let cartx = 0;  // cart position
+let cartv = 0;  // cart velocity
+let theta = 0.1;  // pendulum angle (0 = straight up, anti-clockwise = positive)
+let omega = 0;  // pendulum angular velocity
+
+// Convenience / derivative values
+const WHEELB = (CARTW - CARTH) * 0.5;
+const WHEELR = CARTH * 0.5;
+const A = pm / (pm + cm);
+const B = A * G;
+const C = A * L;
+const D = A / pm;
+const E = G / L;
+const F = D / L;
 
 function setup() {
 	createCanvas(DIMX, DIMY);
 	strokeWeight(1);
 	stroke(255);
 	noFill();
-	angleMode(DEGREES);
 	rectMode(CENTER);
 	ellipseMode(RADIUS);
-
-	cart = new Cart(0, 0, 0, 0, 10, 0.2);
-	pend = new Pendulum(0, 1, 0, 0, 1, 2);
-
-	// Set independent forces
-	cart.setforce();
-	pend.setforce();
-
-	// Force from pend on cart = component in direction of cart of independent force on pend
-	const F = p5.Vector.mult(pend.pos, pend.force.dot(pend.pos));
-	if (cart.pos.x <= 0 && F.y < 0) {
-		//F.y = 0;
-	}
-	cart.addforce(F);
-	pend.addforce(p5.Vector.mult(pend.pos, cart.force.dot(pend.pos)));
 }
 
 function draw() {
 	// Define grid
 	translate(DIMX * 0.5, DIMY * 0.5);  // origin at the centre
 	scale(1, -1);   // positive y goes up, keep pixel scaling
-
-	// Draw scene
 	background(0);  // black background
-	cart.show();
-	pend.show();
+	
+	// Draw cart
+	push();
+	translate(cartx * SCALE, 0);
+	rect(0, 0, CARTW, CARTH);
+	arc(-WHEELB, -WHEELR, WHEELR, WHEELR, PI, 0);
+	arc( WHEELB, -WHEELR, WHEELR, WHEELR, PI, 0);
+	pop();
 
-	// Set independent forces
-	// (gravity + optional external force)
-	// cart.setforce();
-	// pend.setforce();
+	// Draw pendulum
+	const px = cos(theta + PI * 0.5) * L * SCALE;
+	const py = sin(theta + PI * 0.5) * L * SCALE;
+	line(cartx * SCALE, 0, px, py);
+	circle(px, py, PENDR);
 
-	// Add reaction forces
-	// pend.pos is normalised vector from cart to pend
-	// so for force pulling on pend: use -pend.pos, but -1*-1=1
-	// cart.addforce(p5.Vector.mult(pend.pos, pend.force.dot(pend.pos)));
-	// pend.addforce(p5.Vector.mult(pend.pos, cart.force.dot(pend.pos)));
+	const cost = cos(theta);
+	const sint = sin(theta);
+	const U = 0;
+	const det = 1 - A * cost * cost;
+	const W2 = A * omega * omega * sint;
+	const carta = (B * sint * cost - L * W2 + D * U) / det;
+	const penda = (E * sint - W2 * cost + F * U * cost) / det;
 
-	// Update positions
-	// cart.update();
-	// pend.update();
+	cartv += carta * DT;
+	cartx += cartv * DT;
+	omega += penda * DT;
+	theta += omega * DT;
 }
