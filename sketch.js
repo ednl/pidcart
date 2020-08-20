f1047e80e783045d14ec70c0ab6a055d3917df1d
ednl <ewoud@dronkert.net>
2020-08-20 14:32:46 +0200

    Mass-proportional size

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 // Physical parameters
-const Mc = 5;        // cart mass [kg]
-const Mp = 1;        // pendulum mass [kg]
-const L  = 1;        // pendulum length [m]
-const G  = 9.81;     // gravitational constant [m/s^2]
+let   Mc  = 2;        // cart mass [kg]
+let   Mp  = 1;        // pendulum mass [kg]
+const L   = 1;        // pendulum length [m]
+const G   = 9.81;     // gravitational constant [m/s^2]
+const CRR = 0.001;    // coefficient of rolling resistance
+const CD  = 1;        // aerodynamic drag coefficient
 
 // Simulation variables (initial state)
 let cartx = 0;       // cart position [m]
@@ -25,24 +27,26 @@ let   Kd =   5 / DT;
 const DIMX  = 1200;  // canvas width
 const DIMY  =  600;  // canvas height
 const SCALE =  200;  // pixels per metre
-const CARTW =   60;  // cart width in pixels
-const CARTH =   20;  // cart height in pixels
-const PENDR =   10;  // pendulum radius in pixels
+let CARTH = Math.pow(Mc / 1000, 1 / 3) * SCALE;  // cart height in pixels
+let CARTW = CARTH * 3;  // cart width in pixels
+let PENDR = Math.pow(Mp / 1000, 1 / 3) * SCALE;  // pendulum radius in pixels
 
 // Visualisation variables
-let minx = cartx;
-let maxx = cartx;
+let minx = 0;
+let maxx = 0;
+let minU = 0;
+let maxU = 0;
 
 // Convenience / derivative values
 const HALFX = DIMX * 0.5;
 const HALFY = DIMY * 0.5;
-const WHEELB = (CARTW - CARTH) * 0.5;
-const WHEELR = CARTH * 0.5;
-const C1 = Mp / (Mp + Mc);
-const C2 = C1 * G;
-const C3 = C1 / Mp;
-const C4 = G / L;
-const C5 = C3 / L;
+let   WHEELB = (CARTW - CARTH) * 0.5;
+let   WHEELR = CARTH * 0.5;
+let   C1 = Mp / (Mp + Mc);
+let   C2 = C1 * G;
+let   C3 = C1 / Mp;
+let   C4 = C3 / L;
+const C5 = G / L;
 const LP = L * SCALE;
 
 // Controller variables
@@ -54,16 +58,30 @@ let err_dif = 0;     // change in error
 let isDown = false;  // is the pendulum in the bootom half?
 
 // Interface variables
-let slideTheta, slideProp, slideInt, slideDif;
+let slideTheta, slideProp, slideInt, slideDif, slideMc, slideMp;
 
 function showVal() {
-	document.getElementById("valTheta").innerHTML = round(theta * 18000 / PI) / 100 + '&deg;';
+	document.getElementById("valTheta").innerHTML = round(theta * 18000 / PI) / 100;
 	document.getElementById("valProp").innerHTML = round(slideProp.value() * 100) / 100;
 	document.getElementById("valInt").innerHTML = round(slideInt.value() * 100) / 100;
 	document.getElementById("valDif").innerHTML = round(slideDif.value() * 100) / 100;
+	document.getElementById("valMc").innerHTML = round(slideMc.value() * 100) / 100;
+	document.getElementById("valMp").innerHTML = round(slideMp.value() * 100) / 100;
 }
 
 function reset() {
+	Mc = slideMc.value();
+	Mp = slideMp.value();
+	CARTH = Math.pow(Mc / 1000, 1 / 3) * SCALE;  // cart height in pixels
+	CARTW = CARTH * 3;  // cart width in pixels
+	PENDR = Math.pow(Mp / 1000, 1 / 3) * SCALE;  // pendulum radius in pixels
+	WHEELB = (CARTW - CARTH) * 0.5;
+	WHEELR = CARTH * 0.5;
+	C1 = Mp / (Mp + Mc);
+	C2 = C1 * G;
+	C3 = C1 / Mp;
+	C4 = C3 / L;
+
 	cartv = 0;
 	cartx = 0;
 	omega = 0;
@@ -71,8 +89,11 @@ function reset() {
 	Kp = slideProp.value();
 	Ki = slideInt.value() * DT;
 	Kd = slideDif.value() / DT;
+
 	minx = 0;
 	maxx = 0;
+	minU = 0;
+	maxU = 0;
 	err_prv = 0;
 	err_sum = 0;
 	isDown = false;
@@ -80,33 +101,42 @@ function reset() {
 }
 
 function setup() {
-	frameRate(FR);
-	createCanvas(DIMX, DIMY);
-	strokeWeight(1);
+	const cnv = createCanvas(DIMX, DIMY);
+	cnv.parent('sketch');
+	strokeWeight(2);
 	stroke(255);
 	noFill();
 	rectMode(CENTER);
 	ellipseMode(RADIUS);
+	frameRate(FR);
 
 	slideTheta = createSlider(0, 180, (HALF_PI - theta) * 180 / PI, 1);
 	slideProp = createSlider(0, 2000, Kp, 2);
 	slideInt = createSlider(0, 1000, Ki / DT, 1);
 	slideDif = createSlider(0, 1000, Kd * DT, 1);
+	slideMc = createSlider(0.01, 10, Mc, 0.01);
+	slideMp = createSlider(0.01, 10, Mp, 0.01);
 
 	slideTheta.parent('cntTheta');
 	slideProp.parent('cntProp');
 	slideInt.parent('cntInt');
 	slideDif.parent('cntDif');
+	slideMc.parent('cntMc');
+	slideMp.parent('cntMp');
 
 	slideTheta.style('width', '1001px');
 	slideProp.style('width', '1001px');
 	slideInt.style('width', '1001px');
 	slideDif.style('width', '1001px');
+	slideMc.style('width', '1001px');
+	slideMp.style('width', '1001px');
 
 	slideTheta.changed(reset);
 	slideProp.changed(reset);
 	slideInt.changed(reset);
 	slideDif.changed(reset);
+	slideMc.changed(reset);
+	slideMp.changed(reset);
 
 	showVal();
 }
@@ -140,7 +170,7 @@ function draw() {
 		const det = 1 - C1 * cost * cost;
 		const W2 = C1 * omega * omega * sint;
 		const carta = (C2 * sint * cost - L * W2 + C3 * U) / det;
-		const penda = (C4 * sint - W2 * cost + C5 * U * cost) / det;
+		const penda = (C5 * sint - W2 * cost + C4 * U * cost) / det;
 
 		// Euler integration
 		cartv += carta * DT;
@@ -149,17 +179,33 @@ function draw() {
 		theta += omega * DT;
 
 		// Range check
-		if (cartx < minx) {
-			minx = cartx;
-		}
-		if (cartx > maxx) {
-			maxx = cartx;
-		}
 		if (theta > PI) {
 			theta -= TWO_PI;
 		}
 		if (theta <= -PI) {
 			theta += TWO_PI;
+		}
+
+		// Range display
+		const curx = round(cartx * 100);
+		const curU = round(U);
+		document.getElementById("curx").innerHTML = curx;
+		document.getElementById("curU").innerHTML = curU;
+		if (curx < minx) {
+			minx = curx;
+			document.getElementById("minx").innerHTML = curx;
+		}
+		if (curx > maxx) {
+			maxx = curx;
+			document.getElementById("maxx").innerHTML = curx;
+		}
+		if (curU < minU) {
+			minU = U;
+			document.getElementById("minU").innerHTML = curU;
+		}
+		if (curU > maxU) {
+			maxU = U;
+			document.getElementById("maxU").innerHTML = curU;
 		}
 	}
 
@@ -168,9 +214,11 @@ function draw() {
 	scale(1, -1);               // positive y goes up, keep pixel scaling
 	background(0);              // clear screen
 	line(-HALFX, 0, HALFX, 0);  // ground
-	line(0, 0, 0, -PENDR);      // origin marker
-	line(minx * SCALE - CARTW * 0.5, WHEELR, minx * SCALE - CARTW * 0.5, WHEELR + CARTH); // range marker
-	line(maxx * SCALE + CARTW * 0.5, WHEELR, maxx * SCALE + CARTW * 0.5, WHEELR + CARTH); // range marker
+	line(0, -HALFY, 0, HALFY);         // origin marker
+	const pminx = minx * 0.01 * SCALE - CARTW * 0.5;
+	const pmaxx = maxx * 0.01 * SCALE + CARTW * 0.5;
+	line(pminx, WHEELR, pminx, WHEELR + CARTH); // range marker
+	line(pmaxx, WHEELR, pmaxx, WHEELR + CARTH); // range marker
 
 	// Draw cart and pendulum from cart position
 	push();
